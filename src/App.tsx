e39fb0890caa219e7a4a39c9c3836c2f264964ad
Kizito <kizito.azubuike1@gmail.com>
2023-11-03 21:24:25 +0100

    complete navbar build

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import './App.css'
+import 'animate.css'
+import Navbar from './components/Navbar'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=' font-manrope p-5 bg-black-950 min-h-[100vh] text-white-100'>
+      <Navbar />
+      <h1 className=' font-manrope font-extrabold animate__animated animate__bounceInLeft'>
+        Hello WealthWise
+      </h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+import { HiOutlineMenuAlt3, HiOutlineXCircle } from 'react-icons/hi'
+
+const Navbar = () => {
+  const [navOpen, setNavOpen] = useState<boolean>(false)
+
+  return (
+    <div>
+      <div className=' w-full max-w-7xl mx-auto p-3 rounded-md  relative'>
+        <div className=' bg-gradient-to-b from-white-100 backdrop-blur-lg via-transparent to-white-100 bg-opacity-5 absolute h-full w-full opacity-20 left-0 top-0 rounded-md z-0'></div>
+        <div className=' flex justify-between items-center z-10 relative'>
+          <h1 className=' font-extrabold text-lg'>
+            Wealth<span>Wise</span>
+          </h1>
+          <div className=' flex gap-3 items-center md:hidden text-sm'>
+            <span>Service</span>
+            <span>Blog</span>
+            <span>About Us</span>
+            <span>Contact</span>
+            <span>Project</span>
+          </div>
+          <div className=' flex gap-2 items-center'>
+            <button className=' px-5 py-2 rounded-sm md:hidden'>Log in</button>
+            <button className='  px-5 py-2 rounded-sm md:hidden'>
+              Get Started
+            </button>
+            <span
+              onClick={() => {
+                setNavOpen(true)
+                console.log('open')
+              }}
+              className=' p-2 cursor-pointer'>
+              <HiOutlineMenuAlt3 />
+            </span>
+          </div>
+        </div>
+        {navOpen && (
+          <div className=' fixed h-[100vh] w-[100vw] left-0 top-0 bg-black-950 flex justify-center items-center z-50'>
+            <button
+              onClick={() => setNavOpen(false)}
+              className=' text-xl absolute top-5 right-5 '>
+              <HiOutlineXCircle size={40} />
+            </button>
+            <div className=' flex flex-col gap-4 items-start text-lg  animate_animated animate__backInDown animate__backOutRight text-center'>
+              <span className=' w-full'>Service</span>
+              <span className=' w-full'>Blog</span>
+              <span className=' w-full'>About Us</span>
+              <span className=' w-full'>Contact</span>
+              <span className=' w-full'>Project</span>
+              <div>
+                <button className=' px-5 py-2 rounded-sm w-full mb-3'>
+                  Log in
+                </button>
+                <button className='  px-5 py-2 rounded-sm w-full'>
+                  Get Started
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
